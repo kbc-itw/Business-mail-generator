df2a7ff92eec25464dadfad874446c05b30b29e4
Your Name <you@example.com>
2017-11-19 23:45:23 +0900

    generator-template-site

--- Morphological analysis/ma_sample.php
+++ Morphological analysis/ma_sample.php
@@ -34,7 +34,8 @@ else {
     $arr_filter = array();
 }
 ?>
-<html><head>
+<html>
+<head>
 <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 <title>テキスト解析デモ - 日本語形態素解析</title>
 </head>
